88c8419e2aa72e06e0c2802fbbdc82755e9dc331
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2018-01-20 16:42:27 +0300

    redis bağlantısı eklendi.

--- server/uses.js
+++ server/uses.js
@@ -1,8 +1,14 @@
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
 
 module.exports = (app) => {
     // oturum modeli
     app.use(session({
+        store: new RedisStore({ // yeni bir redis bağlantısı
+            host: 'localhost',
+            port: 6379,
+            ttl :  260
+        }),
         secret: "gizli-anahtar",
         resave: false,
         saveUninitialized: true,
